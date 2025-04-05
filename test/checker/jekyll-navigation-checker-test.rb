e6713338bf3bcb14855b4b66fefee6696ce8fcd0
Sebastien Torres <baztorres@gmail.com>
2025-04-05 18:04:11 +0100

    Test file placeholder

---
+++ test/checker/jekyll-navigation-checker-test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'lib/checker/jekyll-navigation-checker'
+require 'minitest/autorun'
+
+class JekyllNavigationCheckerTest < Minitest::Test
+
+  test "test someting" do
+    assert false
+  end
+end
