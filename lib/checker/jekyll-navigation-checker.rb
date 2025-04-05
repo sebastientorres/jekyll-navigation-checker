cb3e33a11bfff2b0ecf26d76696e0df1d28f0908
Sebastien Torres <baztorres@gmail.com>
2025-04-05 18:04:31 +0100

    Method placeholders in jekyll-navigation-checker.rb

--- lib/checker/jekyll-navigation-checker.rb
+++ lib/checker/jekyll-navigation-checker.rb
@@ -2,4 +2,21 @@ class JekyllNavigationChecker < HTMLProofer::Check
   def self.hi
     puts "hi"
   end
+
+  def run
+
+  end # end of run
+
+  def doesFileExist(file)
+    return !File.exist?(file)
+  end
+
+  def doesFileHaveContent(file)
+    return File.size(file) < 1
+  end
+
+  def doesNavigationYmlMatchPermalinks(file)
+    navigationYmlUrls = getUrlsFromNavigationYml(file)
+
+  end
 end
