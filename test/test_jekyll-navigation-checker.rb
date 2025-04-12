a56e3ae50698283acfec0f386aabf3cd23f0c160
Sebastien Torres <baztorres@gmail.com>
2025-04-12 22:41:21 +0100

    Uncomment remaining tests

--- test/test_jekyll-navigation-checker.rb
+++ test/test_jekyll-navigation-checker.rb
@@ -33,28 +33,28 @@ class JekyllNavigationCheckerTest < Minitest::Test
       assert_equal(actual, expected)
     end
   end
-#
-#   def doesFileHaveContents
-#     assert_equal JekyllNavigationChecker.doesFileHaveContent(nil), true
-#   end
-#
-#   NAVIGTIONYMLURLSCASES = {
-#     '../resources/_data/bad-links-dont-end-with-slash' => false,
-#     '../resources/_data/good-links-end-wth-slash' => true,
-#     '../resources/_data/mixed-some-links-dont-end-with-slash' => false,
-#     '../resources/_data/non-existent file' => false
-#   }
-#
-#   def doNavigationYmlUrlsEndInSlash
-#     NAVIGTIONYMLURLSCASES.each do |path, expected|
-#       puts path
-#       file = File.new(path, 'r')
-#       actual = JekyllNavigationChecker.doNavigationYmlUrlsEndInSlash(file)
-#       assert_equal(actual, expected)
-#     end
-#   end
-#
-#   def doesNavigationYmlMatchPermalinks
-#     assert_equal JekyllNavigationChecker.doesNavigationYmlMatchPermalinks(nil), true
-#   end
+
+  def test_doesFileHaveContents
+    assert_equal JekyllNavigationChecker.doesFileHaveContent(nil), true
+  end
+
+  NAVIGTIONYMLURLSCASES = {
+    'test/resources/_data/bad-links-dont-end-with-slash' => false,
+    'test/resources/_data/good-links-end-wth-slash' => true,
+    'test/resources/_data/mixed-some-links-dont-end-with-slash' => false,
+    'test/resources/_data/non-existent file' => false
+  }
+
+  def test_doNavigationYmlUrlsEndInSlash
+    NAVIGTIONYMLURLSCASES.each do |path, expected|
+
+      file = File.new(path, 'r')
+      actual = JekyllNavigationChecker.doNavigationYmlUrlsEndInSlash(file)
+      assert_equal(actual, expected)
+    end
+  end
+
+  def test_doesNavigationYmlMatchPermalinks
+    assert_equal JekyllNavigationChecker.doesNavigationYmlMatchPermalinks(nil), true
+  end
 end
